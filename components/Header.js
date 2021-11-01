d2d697395aa384a819e0ef03ea482297afda2152
andrejarboe <andrejarboe@gmail.com>
2021-11-01 14:58:21 -0400

    fix.map

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,12 @@
 //./components/Header.js
 import Image from 'next/image';
-import { SearchIcon } from "@heroicons/"
+import { 
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    SearchIcon,
+    UsersIcon, 
+} from "@heroicons/react/solid";
 
 function Header() {
     return (
@@ -16,15 +22,28 @@ function Header() {
             </div>
             {/* left end */}
             {/* middle - search */}
-            <div className="">
-                <input type="text" placeholder="Start your search" />
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
                 <SearchIcon 
-                    className="h-8"
+                    className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-auto md:mx-2"
                 />
             </div>
             {/* middle end */}
             {/* right */}
-            <div className=""></div>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
+                <GlobeAltIcon 
+                    className="h-6"
+                />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+                    <MenuIcon 
+                        className="h-6"
+                    />
+                    <UserCircleIcon 
+                        className="h-6"
+                    />
+                </div>
+            </div>
             {/* right end */}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -13,7 +14,33 @@ export default function Home() {
       <Header />
       {/* Header end*/}
         
-      {/* Banner */}
+      {/* Hero */}
+      <Hero />
+      {/* Hero end */}
+      {/* main */}
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Pull some data from the server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
+      {/* main end */}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('http://links.papareact.com/pyp')
+    .then(
+      (res) => res.json()
+    );
+
+    return {
+      props:{
+        exploreData
+      }
+    }
 }
