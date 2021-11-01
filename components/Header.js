dc25a96bffc664c768719175acb4f9f24baec0be
andrejarboe <andrejarboe@gmail.com>
2021-11-01 13:59:37 -0400

    npm install @heroicons/vue

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,21 @@ import Image from 'next/image';
 
 function Header() {
     return (
-        <div>
+        <div className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
             {/* left  */}
-            <div className="">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto ">
                 <Image
                     src="http://links.papareact.com/qd3"
                     layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
                 />
             </div>
             {/* left end */}
-            {/* middle  */}
-            <div className=""></div>
+            {/* middle - search */}
+            <div className="">
+                
+            </div>
             {/* middle end */}
             {/* right */}
             <div className=""></div>
