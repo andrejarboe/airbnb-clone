54b0ec4dd812d5e7d8ee16c8bd9ec8b35f6dcd10
andrejarboe <andrejarboe@gmail.com>
2021-11-01 13:48:23 -0400

    start header

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+//./components/Header.js
+import Image from 'next/image';
+
+function Header() {
+    return (
+        <div>
+            {/* left  */}
+            <div className="">
+                <Image
+                    src="http://links.papareact.com/qd3"
+                    layout="fill"
+                />
+            </div>
+            {/* left end */}
+            {/* middle  */}
+            <div className=""></div>
+            {/* middle end */}
+            {/* right */}
+            <div className=""></div>
+            {/* right end */}
+        </div>
+    )
+}
+
+export default Header
