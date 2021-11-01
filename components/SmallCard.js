15acd52c3e3381493019ddd24239598442d797da
andrejarboe <andrejarboe@gmail.com>
2021-11-01 16:13:11 -0400

    large card

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,9 +1,26 @@
-function SmallCard() {
-    return (
-        <div className="">
-            
-        </div>
-    )
+// ./component/SmallCard,js
+import Image from 'next/image';
+
+function SmallCard({ img, location, distance }) {
+  return (
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+      {/* left */}
+      <div className="relative h-16 w-16">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          className="rounded-lg"
+        />
+      </div>
+      {/* right */}
+      <div className="">
+          <h2 className="">{location}</h2>
+          <h3 className="text-gray-500">{distance}</h3>
+      </div>
+    </div>
+  );
 }
 
-export default SmallCard
+export default SmallCard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -13,7 +16,7 @@ export default function Home({ exploreData }) {
       {/* Header */}
       <Header />
       {/* Header end*/}
-        
+
       {/* Hero */}
       <Hero />
       {/* Hero end */}
@@ -22,10 +25,36 @@ export default function Home({ exploreData }) {
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           {/* Pull some data from the server - API endpoints */}
-          {exploreData?.map((item) => (
-            <h1>{item.location}</h1>
+          {exploreData?.map(({ img, location, distance }) => (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            </div>
           ))}
         </section>
+
+        {/* Medium card */}
+        <div className="section">
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </div>
+        {/* Medium card end */}
+        {/* large card */}
+        <LargeCard 
+          img="http://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Whishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
+        {/* Large card end */}
       </main>
       {/* main end */}
     </div>
@@ -33,14 +62,18 @@ export default function Home({ exploreData }) {
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch('http://links.papareact.com/pyp')
-    .then(
-      (res) => res.json()
-    );
+  const exploreData = await fetch('http://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
 
-    return {
-      props:{
-        exploreData
-      }
-    }
+  const cardsData = await fetch('http://links.papareact.com/zp1').then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
